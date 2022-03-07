232c61a2184dc8dec79d354bee4ad81f68f7cff5
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2022-03-07 13:26:11 +0700

    Rename drive to storage

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const osu = require('node-os-utils')
 
 module.exports = async (req, res) => {
-  const [cpu, mem, drive, network] = await Promise.all([
+  const [cpu, mem, storage, network] = await Promise.all([
     osu.cpu.usage(),
     osu.mem.info(),
     osu.drive.info(),
     osu.netstat.inOut(),
   ])
 
-  return { cpu, mem, drive, network: network && network.total }
+  return { cpu, mem, storage, network: network && network.total }
 }
