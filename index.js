3c5b1c313d90cb3c127f30e44ecd7b0531d8528c
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2022-03-10 12:24:45 +0700

    Add hive and lcd health check

--- index.js
+++ index.js
@@ -1,12 +1,77 @@
 const osu = require('node-os-utils')
+const { send } = require('micro')
+const { router, get } = require('microrouter')
+const fetch = require('cross-fetch')
 
-module.exports = async (req, res) => {
-  const [cpu, mem, storage, network] = await Promise.all([
+/************************************************
+ * Utilities
+ ***********************************************/
+
+const healthCheckHive = async () => {
+  try {
+    const res = await fetch(
+      'http://localhost:8085/.well-known/apollo/server-health',
+    )
+
+    if (res.status >= 400) return 'Hive failed to respond!'
+  } catch {
+    return 'Hive failed to respond!'
+  }
+}
+
+const healthCheckLCD = async () => {
+  try {
+    const res = await fetch('http://localhost:1317/syncing')
+
+    if (res.status >= 400) return 'LCD failed to respond!'
+
+    const lcdJson = await res.json()
+
+    if (json.syncing) return 'LCD is catching up!'
+  } catch {
+    return 'LCD failed to respond!'
+  }
+}
+
+/************************************************
+ * Route handlers
+ ***********************************************/
+
+const machine = async (req, res) => {
+  const [cpu, mem, storage, _network] = await Promise.all([
     osu.cpu.usage(),
     osu.mem.info(),
     osu.drive.info(),
     osu.netstat.inOut(),
   ])
 
-  return { cpu, mem, storage, network: network && network.total }
+  const network = _network && _network.total
+
+  return send(res, 200, { cpu, mem, storage, network })
 }
+
+const hiveHealth = async (req, res) => {
+  const messages = []
+
+  const errors = await Promise.all([healthCheckHive(), healthCheckLCD()])
+
+  messages.push(...errors.filter(Boolean))
+
+  const healthy = messages.length === 0
+
+  return send(res, healthy ? 200 : 500, healthy ? 'Healthy' : messages)
+}
+
+const lcdHealth = async (req, res) => {
+  const lcdError = await healthCheckLCD()
+  return send(res, lcdError ? 500 : 200, lcdError || 'Healthy')
+}
+
+const notfound = (req, res) => send(res, 404, '404 Not Found')
+
+module.exports = router(
+  get('/machine', machine),
+  get('/hive-health', hiveHealth),
+  get('/lcd-health', lcdHealth),
+  get('/*', notfound),
+)
